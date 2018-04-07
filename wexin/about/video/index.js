58545c1d2620cc58daf1cf7fc0be49130427f25c
huhu <540543191@qq.com>
2018-04-07 14:10:18 +0800

    添加视频

--- wexin/about/video/index.js
+++ wexin/about/video/index.js
@@ -1,1 +1,20 @@
-Page({})
+Page({
+    data: {
+        heightStyle: ""
+    },
+    onLoad: function(){
+        var that = this;
+        var systemInfo = wx.getSystemInfoSync();
+        var heightStyle = 'height:' + systemInfo.windowHeight + 'px';
+        that.setData({
+            heightStyle: heightStyle
+        })
+    },
+    onShareAppMessage: function(){
+        return {
+            title: 'Computer',
+            desc: 'computer video',
+            path: '/about/video/index'
+        }
+    }
+})
